52b98f0023bfb38d042964e3c7bb930791c4593a
Samantha Diaz <samidiaz3@gmail.com>
2022-02-01 00:39:44 -0500

    commit tree 71b8dfdec9f724bc36043b27d21f8ed581f415be

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
-  //add static methods here
+
+  static capitalize(string) {
+    return string.charAt( 0 ).toUpperCase() + string.slice( 1 )
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9 '-]+/g, '' )
+  }
+  
+  static titleize(string){
+    Formatter.capitalize(string)
+
+    let arr = string.split(' ')
+    let exludeWords = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+
+    // https://stackoverflow.com/questions/60393691/capitalize-every-first-word-except-some
+    let newArray =  arr.map((word, i) => {
+      return exludeWords.includes(word) && i!=0 ? [word] : word.charAt(0).toUpperCase() + word.slice(1);
+    })
+    
+    return newArray.join(' ')
+
+  }
 }
